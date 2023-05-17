c8fde5f24fbaa4d190c5c038cc4b5cd1de9c4531
Lycosidae <shadyassaad0@gmail.com>
2023-05-17 16:48:51 -0400

    added mouse click with right player

--- src/app/component/gameboard/gameboard.component.ts
+++ src/app/component/gameboard/gameboard.component.ts
@@ -9,6 +9,9 @@ export class GameboardComponent implements OnInit {
   square: string[];
   currentPlayer: string;
   isPlayerXNext: boolean;
+  winner: boolean;
+  row: string[3][3];
+  column: string[3][3];
 
   get GameSquare() {
     return this.square;
@@ -16,8 +19,11 @@ export class GameboardComponent implements OnInit {
 
   ngOnInit() {
     this.square = Array(9).fill(null);
+    // this.row = Array(3).fill(null);
+    // this.column = Array(3).fill(null);
     this.currentPlayer = 'X';
     this.isPlayerXNext = false;
+    this.winner = false;
   }
 
   setCurrentPlayer() {
@@ -25,8 +31,34 @@ export class GameboardComponent implements OnInit {
   }
 
   addShape(squareChosen: number) {
-    this.square[squareChosen] = this.currentPlayer;
-    this.isPlayerXNext = this.currentPlayer == 'X' ? false : true;
-    this.setCurrentPlayer();
+    if (this.square[squareChosen] == null) {
+      this.square[squareChosen] = this.currentPlayer;
+      this.isPlayerXNext = this.currentPlayer == 'X' ? false : true;
+      this.setCurrentPlayer();
+    }  
+  }
+
+  checkWinner() {
+    this.winner  = this.checkRow();
+  }
+
+  checkRow(): boolean {
+    let verify: number = 0;
+    let currentValue: string = '';
+    for(let i: number = 0; i <= this.row.length; i++) {
+      for(let j: number = 0; j <= this.row[i].length; j++) {
+        if (this.row[i][j] != null) {
+          currentValue = this.square[i];
+          verify++;
+          if (verify == 3) return true;
+        }
+      }
+      verify = 0;
+    }
+    return false;
+  }
+
+  checkColumn(): boolean {
+    return false;
   }
 }
